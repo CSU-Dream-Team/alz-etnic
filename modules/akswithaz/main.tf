64f58d1f87f8ce176574b665de34f5c8f7cf94e0
Jamel Achahbar <jachahbar@microsoft.com>
2024-07-25 15:59:31 +0200

    add aks

---
+++ modules/akswithaz/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0, < 4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = "East US 2" # Hardcoded because we have to test in a region with availability zones
+  name     = module.naming.resource_group.name_unique
+}
+
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = ">= 0.3.0"
+}
+
+resource "azurerm_user_assigned_identity" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = "uami-${var.kubernetes_cluster_name}"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+# Do not specify location here due to the randomization above.
+# Leaving location as `null` will cause the module to use the resource group location
+# with a data source.
+module "test" {
+  source  = "Azure/avm-ptn-aks-production/azurerm//examples/with_availability_zone"
+  version = "0.1.0"
+  kubernetes_version  = "1.28"
+  enable_telemetry    = var.enable_telemetry # see variables.tf
+  name                = module.naming.kubernetes_cluster.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  managed_identities = {
+    user_assigned_resource_ids = [
+      azurerm_user_assigned_identity.this.id
+    ]
+  }
+
+  location = "East US 2" # Hardcoded because we have to test in a region with availability zones
+  node_pools = {
+    workload = {
+      name                 = "workload"
+      vm_size              = "Standard_D2d_v5"
+      orchestrator_version = "1.28"
+      max_count            = 110
+      min_count            = 2
+      os_sku               = "Ubuntu"
+      mode                 = "User"
+    },
+    ingress = {
+      name                 = "ingress"
+      vm_size              = "Standard_D2d_v5"
+      orchestrator_version = "1.28"
+      max_count            = 4
+      min_count            = 2
+      os_sku               = "Ubuntu"
+      mode                 = "User"
+    }
+  }
+}
